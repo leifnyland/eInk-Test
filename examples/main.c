f215e195a149b8f8f9fad7327f3e481728d19903
Leif Nyland <leif.nyland@gmail.com>
2026-08-10 18:59:07 -0400

    IT WORKS! Hello World completed

--- examples/main.c
+++ examples/main.c
@@ -5,7 +5,7 @@
 #include "GUI_Paint.h"
 #include "GUI_BMPfile.h"
 #include "Debug.h"
-#include "EPD_5in83_V2.h"
+#include "EPD_5in83g.h"
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -27,33 +27,45 @@ int main()
         return -1;
     }
 	printf("Init and Clear\r\n");
-    EPD_5in83_V2_Init();
-    EPD_5in83_V2_Clear();  // clear panel before drawing, not after
+    EPD_5IN83G_Init();
+    EPD_5IN83G_Clear(EPD_5IN83G_WHITE);  // clear panel before drawing, not after
+	DEV_Delay_ms(2000);
 
     // 1. Allocate a buffer for the image
     UBYTE *BlackImage;
-    UWORD Imagesize = ((EPD_5in83_V2_WIDTH % 8 == 0)? (EPD_5in83_V2_WIDTH / 8 ): (EPD_5in83_V2_WIDTH / 8 + 1)) * EPD_5in83_V2_HEIGHT;
+    UDOUBLE Imagesize = ((EPD_5IN83G_WIDTH % 4 == 0)? (EPD_5IN83G_WIDTH / 4 ): (EPD_5IN83G_WIDTH / 4 + 1)) * EPD_5IN83G_HEIGHT;
     if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
         printf("Failed to apply for black memory...\r\n");
         return -1;
     }
 
     // 2. Tell Paint library about this buffer
-	EPD_5in83_V2_Init();
-    Paint_NewImage(BlackImage, EPD_5in83_V2_WIDTH, EPD_5in83_V2_HEIGHT, 0, WHITE);
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
+	printf("Paint_NewImage\r\n");
+    Paint_NewImage(BlackImage, EPD_5IN83G_WIDTH, EPD_5IN83G_HEIGHT, 0, EPD_5IN83G_WHITE);
+	Paint_SetScale(4);
+	Paint_SelectImage(BlackImage);
+    Paint_Clear(EPD_5IN83G_WHITE);
 
     // 3. Now draw into it
-    Paint_DrawString_EN(10, 10, "hello world!", &Font16, BLACK, WHITE);
+    Paint_DrawString_EN(10, 10, "hello world!", &Font16, EPD_5IN83G_WHITE, EPD_5IN83G_BLACK);
 
     // 4. Send buffer to the actual display
-    EPD_5in83_V2_Display(BlackImage);
+    EPD_5IN83G_Display(BlackImage);
+	DEV_Delay_ms(5000);
 
-    DEV_Delay_ms(5000);
-    EPD_5in83_V2_Clear();
-    DEV_Module_Exit();
+	printf("Clearing\r\n");
+	EPD_5IN83G_Init();
+    EPD_5IN83G_Clear(EPD_5IN83G_WHITE);
+	DEV_Delay_ms(2000);
+
+    printf("Goto Sleep...\r\n");
+    EPD_5IN83G_Sleep();
     free(BlackImage);
     BlackImage = NULL;
+    DEV_Delay_ms(2000);//important, at least 2s
+    // close 5V
+    printf("close 5V, Module enters 0 power consumption ...\r\n");
+    DEV_Module_Exit();
+
     return 0;
 }
