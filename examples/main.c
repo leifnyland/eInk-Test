0dce8514d97f2033431e96a3a559e77bf333d4a6
Leif Nyland <leif.nyland@gmail.com>
2026-08-09 22:44:59 -0600

    fist test

---
+++ examples/main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>     //exit()
+#include <signal.h>     //signal()
+#include "EPD_Test.h"   //Examples
+#include <unistd.h>
+
+void  Handler(int signo)
+{
+    //System Exit
+    printf("\r\nHandler:exit\r\n");
+    DEV_Module_Exit();
+
+    exit(0);
+}
+
+int main()
+{
+    // Exception handling:ctrl + c
+    signal(SIGINT, Handler);
+    if(DEV_Module_Init()!=0){
+        return -1;
+    }
+	printf("e-Paper Init and Clear...\r\n");
+    EPD_5in83_V2_Init();
+
+	Paint_DrawString_EN(10, 10, "hello world!", &Font16, BLACK, WHITE)
+
+	sleep(10);
+	EPD_5in83_V2_Clear();
+	DEV_Module_Exit();
+	return 0;
+}
